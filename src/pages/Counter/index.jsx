0bd4fc62d08b64c9871b23764d1103b6cc670abe
khuong dep trai <khuongduyle.it@gmail.com>
2025-09-03 10:49:41 +0700

    complete Count and Todo components

--- src/pages/Counter/index.jsx
+++ src/pages/Counter/index.jsx
@@ -1,15 +1,52 @@
 import { useState } from "react";
 import Button from "../Buttons";
+import styles from "./Counter.module.scss"
 
 function Counter() {
     const [count, setCount] = useState(0)
     return (
-        <>
-            <h1>{count}</h1>
-            <Button
-                onClick={() => setCount(count + 1)}
-            >increase</Button>
-        </>
+        <div className={styles.app}>
+            <div
+                style={{ color: count > 0 ? 'green' : count < 0 ? 'red' : 'gray' }}
+                className={styles.counter}
+            >
+                {count}
+            </div>
+
+            <div
+                className={styles['count-status']}
+                style={{ color: count > 0 ? 'green' : count < 0 ? 'red' : 'gray' }}
+            >
+                {count > 0 ? "Dương" : count < 0 ? "Âm" : "Bằng không"}
+            </div>
+
+            <div className={styles['button-container']}>
+                <Button
+                    rounded
+                    size="large"
+                    className={styles['btn-margin']}
+                    onClick={() => setCount(count - 1)}
+                >
+                    Decrease
+                </Button>
+                <Button
+                    rounded
+                    size="large"
+                    className={styles['btn-margin']}
+                    onClick={() => setCount(count + 1)}
+                >
+                    Increase
+                </Button>
+                <Button
+                    rounded
+                    size="large"
+                    className={styles['btn-margin']}
+                    onClick={() => setCount(0)}
+                >
+                    Reset
+                </Button>
+            </div>
+        </div>
     )
 }
 
--- src/pages/Todo/index.jsx
+++ src/pages/Todo/index.jsx
@@ -1,9 +1,117 @@
+import { useState } from "react";
+
+import styles from "./Todo.module.scss"
+import Button from "../Buttons";
+
+let uniqId = -1;
+
 function Todo() {
+    const [inputValue, setInputValue] = useState('');
+    const [todos, setTodos] = useState([]);
+    const [edited, setEdited] = useState(false);
+    const [editId, setEditId] = useState()
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value); // Lấy giá trị từ input
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // Ngăn trang reload khi submit form
+        if (inputValue.trim()) {
+            if (!edited) {
+                setTodos([...todos, { id: ++uniqId, text: inputValue, completed: false }]);
+                setInputValue(''); // Reset input sau khi thêm
+            } else {
+                handleEditTodo(editId);
+                setInputValue(''); // Reset input sau khi thêm
+            }
+        }
+    };
+
+    const handleCheckedTodo = (id) => {
+        const newTodos = todos.map(todo =>
+            todo.id === id ? { ...todo, completed: !todo.completed } : todo
+        );
+        setTodos(newTodos);
+    }
+
+    const handleDeleteTodo = (id) => {
+        const isConfirm = confirm('Bạn có muốn xóa công việc này không !')
+        if (isConfirm) {
+            const newTodos = todos.filter(todo => todo.id !== id)
+            setTodos(newTodos)
+        }
+    }
+
+    const handleEditTodo = (id) => {
+        const newTodos = todos.map(todo =>
+            todo.id === id ? { ...todo, text: inputValue } : todo
+        );
+        setTodos(newTodos);
+        setEdited(false);
+        setEditId(null);
+    }
+
     return (
-        <>
-            <h1>Todo page</h1>
-        </>
-    )
+        <div className={styles.app}>
+            <form onSubmit={handleSubmit}>
+                <input
+                    className={styles['input-todo']}
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    placeholder="Nhập task mới..."
+                />
+                <Button type="submit">
+                    {edited ? 'Sửa' : 'Thêm'}
+                </Button>
+            </form>
+
+            <div className={styles['task-wrapper']}>
+                <div className={styles['task-list']}>
+                    {todos.map((todo) => (
+                        <div
+                            className={`${styles['task-item']} ${todo.completed ? styles.completed : ""}`}
+                            style={{ display: 'flex' }}
+                            key={todo.id}>
+                            <div className={styles['task-item--first']}>
+                                <div className={styles['btn--align-center']}>
+                                    <div onClick={() => handleCheckedTodo(todo.id)}>
+                                        {todo.completed ?
+                                            <i className="fa-solid fa-square-check"></i> :
+                                            <i className="fa-regular fa-square"></i>
+                                        }
+                                    </div>
+                                </div>
+
+                                <p className={styles['task-item__text']}>{todo.text}</p>
+                            </div>
+
+                            <div className={styles['task-item--control']}>
+                                <div
+                                    onClick={() => {
+                                        setInputValue(todo.text);
+                                        setEditId(todo.id)
+                                        setEdited(!edited)
+                                    }}
+                                >
+                                    <i className="fa-solid fa-pen-to-square"></i>
+                                </div>
+
+                                <div
+                                    onClick={() => handleDeleteTodo(todo.id)}
+                                >
+                                    <i className="fa-solid fa-trash"></i>
+                                </div>
+                            </div>
+
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+
+        </div>
+    );
 }
 
 export default Todo;
