3bf25c27f53edec5fa9445db7e1e54628d28d624
khuong dep trai <khuongduyle.it@gmail.com>
2025-09-04 14:34:51 +0700

    finish Profile and Products components

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,8 +1,81 @@
-function Products() {
+import { useEffect, useState } from "react";
+
+import styles from "./Products.module.scss"
+import Button from "../Buttons";
+
+function Modal({ product, onClose }) {
+    if (!product) return null
     return (
-        <>
-            <h1>Products page</h1>
-        </>
+        <div className={styles.modalOverlay}>
+            <div className={styles.boxModal}>
+                <div className={styles.header}>
+                    <p><strong>ID bài viết: </strong>{product.id}</p>
+                    <div className={styles.icon} onClick={onClose} >
+                        <i className="fa-solid fa-xmark"></i>
+                    </div>
+                </div>
+                <div className={styles.content}>
+                    <div className={styles.title}>{product.title}</div>
+                    <p className={styles.textContent}>{product.body}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function Products() {
+    const [products, setProducts] = useState([]);
+    const [showModal, setShowModal] = useState(null);
+
+    const handleShortenText = (text) => {
+        if (text.length > 100) {
+            const textCopy = text.slice(0, 100)
+            return `${textCopy}...`
+        } else
+            return text;
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+                .then(res => res.json())
+                .then(data => setProducts(data))
+                .catch(err => console.error("Error: ", err));
+        }, 800)
+    }, [])
+
+    if (products.length === 0) {
+        return (
+            <div className={styles.app}>
+                <p className={styles.loader}>.</p>
+            </div>)
+    }
+
+    return (
+        <div className={styles.app}>
+            <div className={styles['product-list']}>
+                {products.map(product => (
+                    <div
+                        className={styles['product-item']}
+                        key={product.id}
+                    >
+                        <p><strong>ID bài viết: </strong>{product.id}</p>
+                        <h3>{product.title}</h3>
+                        <p>{handleShortenText(product.body)}</p>
+                        <Button
+                            className={styles['button-custom']}
+                            onClick={() => setShowModal(product)}
+                        >
+                            Xem chi tiết
+                        </Button>
+                    </div>
+                ))}
+            </div>
+            <Modal
+                product={showModal}
+                onClose={() => setShowModal(null)}
+            ></Modal>
+        </div>
     )
 }
 
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,8 +1,46 @@
+import { useEffect, useState } from "react";
+import styles from "./Profile.module.scss"
+
+
 function Profile() {
+    const [user, setUser] = useState({})
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('https://jsonplaceholder.typicode.com/users/1')
+                .then(res => res.json())
+                .then(data => setUser(data))
+                .catch(err => console.warn(err))
+        }, 800)
+    }, [])
+
+    if (!user || !user.address) {
+        return (
+            <div className={styles.app}>
+                <p className={styles.loader}>.</p>
+            </div>)
+    }
+
     return (
-        <>
-            <h1>Profile page</h1>
-        </>
+        <div className={styles.app}>
+            <div className={styles.wrapper}>
+                <div className={styles['item-center']}>
+                    <img
+                        className={styles['card-img']}
+                        src="https://ui-avatars.com/api/?name=Leanne+Graham&&background=0D8ABC&color=fff" alt=""
+                    />
+                </div>
+                <h2 className={styles['text-center']}>{user.name}</h2>
+                <p><strong>Username:</strong> {user.username}</p>
+                <p><strong>Email:</strong> {user.email}</p>
+                <p><strong>Phone:</strong> {user.phone}</p>
+                <p><strong>Website:</strong> {user.website}</p>
+                <p>
+                    <strong>Address:</strong> {user.address.street}, {user.address.city}
+                </p>
+            </div>
+
+        </div >
     )
 }
 
